1ff51d9c766d5e35582adc93512a3f0bc4777a9b
nathb2001 <nathan.bishop1@outlook.com>
2024-07-15 21:17:50 +0100

    Added LLM and updated analytics

--- Home.py
+++ Home.py
@@ -19,26 +19,63 @@ nltk.download('wordnet')
 import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.decomposition import LatentDirichletAllocation as LDA
-
 from streamlit_gsheets import GSheetsConnection
 from datetime import date
-
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain.agents import Tool, load_tools, initialize_agent, AgentType
 
+#getting llm model from groq api
 chat = ChatGroq(temperature=0, groq_api_key=st.secrets.ChatGroq.groq_key,model_name="llama3-8b-8192")
-#loading ml model
+
 @st.cache_resource
 def load_model():
-    with open('regression_model.joblib', 'rb') as joblib_in:
+    '''
+    loads fake news model (trained logistic regression model)
+    '''
+    with open('fakenews_model.joblib', 'rb') as joblib_in:
         model = joblib.load(joblib_in)
     return model
+def check_db(text):
+    '''checks if text exists in database by using exception handling if not exists will be run 
+    through ml and llm model as an error will be raised. If it exists the information from db gets 
+    fetched and displayed to the user
+    '''
+    conn = st.connection("gsheets",type=GSheetsConnection)
+    try:
+      text=str(text)
+      text = text.replace("'", "''")
+      sqlQuery=f"SELECT EXISTS(SELECT 1 FROM Sheet1 where Article = '{text}') AS news_exist" 
+      select=conn.query(sql=sqlQuery,ttl=20)
+      sql=f"SELECT * FROM Sheet1 WHERE Article ='{text}'" #uses sql select statement to get updated result
+      select=conn.query(sql=sql,ttl=20)
+      classification = select['Classification'].loc[0].upper()
+      llm = select['LLM'].loc[0]
+      topics = select['Topic'].loc[0]
+      sentiment = select['Sentiment'].loc[0]
+      sent_dict={'Positive':':green[**Positive**]','Negative':':red[**Negative**]','Neutral':'[**Neutral**]'}#streamlit doesnt support yellow
 
+      if classification == 'Real':
+         colour = ':green'
+      else:
+         colour = ':red' 
+      st.markdown(f'We have already classified this article and found it was {colour}[**{classification}**]') # need to work out the colour c 
+      st.markdown('Our Large Language model has Fact-Checked the claims and found:')
+      st.write(llm)
+      st.write(f'Additionally we found that this news article with the keywords of "{topics}" has a {sent_dict[sentiment]} sentiment')
+    except:
+      return False
+   
 def scrape(text):
+  '''
+  uses libraries of requests to load the page and bs4 to parse the html text. This goes to a LLM 
+  which will only get the article contents and return it back
+  '''
   page = requests.get(text)
   soup = BeautifulSoup(page.content, "html.parser")
   article = soup.text
@@ -49,18 +86,19 @@ def scrape(text):
   for chunk in chain.stream({"article": q}):
       claim = (chunk.content)
       varr.append(claim)
-
   claims = ''.join(varr)
   claims=claims.replace('\n','')
   return claims
 
-#function for getting data and making prediction
 @st.cache_data() 
 def predict(data):
+    '''
+    Loads logistic regression model, turns user input article to dataframe and preprocesses it.
+    As training dataset it large it gets loaded in parts then merged. Text gets vectorised through 
+    TF-IDF then model makes prediction and user gets shown result. 
+    Function returns data, classification and date.    
+    '''
     model = load_model()
-    sentiment = get_sentiment(data)
-    dlist = [data]
-    topics = topic(dlist)
     data2 = pd.DataFrame([data], columns = ['Statement'])
     data2['Statement'] = preprocess(data2['Statement'])
 
@@ -91,31 +129,72 @@ def predict(data):
     Fake = round(Fake*100,1)
     Real = round(Real*100,1)
 
-    Real_Msg = f'REAL! We predicted that the probability this News article is Real is {Real} percent'
-    Fake_Msg = f'FAKE! We predicted that the probability this News article is Fake is {Fake} percent'
+    Real_Msg = f':green[**REAL!**] We predicted that the probability this News article is :green[**Real**] is {Real} percent'
+    Fake_Msg = f':red[**FAKE!**] We predicted that the probability this News article is :red[**Fake**] is {Fake} percent'
     classification=''
     if predictions == [1]:
-        st.write(Real_Msg)
+        st.markdown(Real_Msg)
         classification+='Real'
     else:
-        st.write (Fake_Msg)
+        st.markdown (Fake_Msg)
         classification+='Fake'
 
-    st.write(f'Additionally we found that this news article with the keywords of "{topics}" has a {sentiment} sentiment')
     datee=date.today()
     stuff=[data,classification,datee]
-    old_data=conn.read()
-    info=pd.DataFrame(data=[stuff],columns=['Article','Classification','Date'])
-    concat_data = pd.concat([old_data, info], ignore_index=True)
-    conn.update(data=concat_data)
+    return stuff
     
-#function for llm
-def llm(text):
-  pass 
+def llm_agent(chat, q):
+  '''
+  This loads the duckduckgo tool and the langchain agent using the LLM of llama 8b.
+  This is the part where the llm will recieve the claims and use the tools to make a decision if
+  real or fake
+  '''
+  search = DuckDuckGoSearchRun()
+  news_tool = Tool(
+        name = "fact check",
+        func=search,
+        description="useful for checking facts from news articles using the internet"
+    )
+  print(f'Inside generate_and_print: q = {q}')
+  tool = [news_tool]
+  agent = initialize_agent(tool, chat,
+                             agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+                             verbose=True,
+                             handle_parsing_errors=True,
+                             agent_kwargs={},max_iterations=50)
+  input = """ Here is a bullet points of claims made from a news article {statement}. You are a fact checker who needs to check these claims I gave you and
+    determine if the article that contains the claims  is fake or real news
+    You need to use the news_tool tool to fact check claims on duckduckgo, IF the news_tool does not provide results for one claim, rephrase what you are searching. after 3 times, IF that does not work,
+    skip to the next claim. If you still are on the same claim after 3 times, your thought needs to go on to the next claim.  When you have fact checked all the claims, reflect on this and decide if the article is fake news if not then it is real news.
+
+    In the explanation right that the Fact Claim checker has concluded if it is Real or if the article is Fake, this is important
+    You need to explain in 50 words why the article is either fake or real. """
+  return agent.run(input.format(statement=q))
+  
+def agent(article):
+  '''
+  LLM gets article and decides the claims from it that need fact checking then sends it to the 
+  llm agent and when gets the decision the result gets displayed and returned.
+  '''
+  q=str(article)
+  varr = []
+  prompt = ChatPromptTemplate.from_messages([("system", "You are an experienced fact checker. Get a few important claims from that you think should be fact checked on the internet but only write the claims and keep it concise and do not repeat claims. do not write stuff that cannot be proven on the internet {claims}")])
+  chain = prompt | chat
+  for chunk in chain.stream({"claims": q}):
+      claim = (chunk.content)
+      varr.append(claim)
+
+  claims = ''.join(varr)
+  result=llm_agent(chat, claims)
+  st.markdown(result)
+  return result
 
 #function for preprocessing data     
 @st.cache_data(show_spinner=False)        
 def preprocess(text):
+  '''
+  Used to preprocess the data so it is normalised and in same format as training data was
+  '''
   df = pd.DataFrame(text,columns=['Statement'])
   df['Statement'] = df['Statement'].str.replace(r'[^\x00-\x7f]_?', r'', regex=True)
   df['Statement'] = df['Statement'].str.replace(r'https?://\S+|www\.\S+', r'', regex=True)
@@ -131,6 +210,9 @@ def preprocess(text):
 #function to get the sentiment of news
 @st.cache_data(show_spinner=False)        
 def get_sentiment(article):
+  '''
+  Gets overall sentiment of article using NTLK sentiment analysis and returns it 
+  '''
   analyzer = SentimentIntensityAnalyzer()
   score = analyzer.polarity_scores(article)
   compound_score = score.get('compound')
@@ -142,11 +224,14 @@ def get_sentiment(article):
     rating = values[1]
   elif compound_score <= -0.5:
     rating = values[2]
-  return rating
+  sent_dict={'Positive':':green[**Positive**]','Negative':':red[**Negative**]','Neutral':'[**Neutral**]'}
+  return rating, sent_dict
 
-#topic modelling
 @st.cache_data(show_spinner=False)        
 def topic(article):
+  '''
+  Topic modelling for the article using Latent Dirichlet Allocation 
+  '''
   text = [preprocess(article)]
   count_vect = CountVectorizer(stop_words=stopwords.words('english'), lowercase=True)
   x_counts = count_vect.fit_transform(text)
@@ -172,50 +257,49 @@ def topic(article):
         words+=y
   return words
 
-
 #the actual website
 st.set_page_config(
     page_title="🏠Home",
     page_icon="🕵️‍♂️",
 )
-conn = st.connection("gsheets",type=GSheetsConnection)
 
-
+conn = st.connection("gsheets",type=GSheetsConnection) #loading connection to db
 st.write("# Fake News Detector 🕵️‍♂️")
-
 text = st.text_input("Enter an Article or an Article Link here:", key="Article")
 st.write('Hint💡: Try to enter as much of the news article contents as possible and to not include information that is not related to the article.')
 
 if text:
-    st.write('Starting a long computation...')
-    
     latest_iteration = st.empty()
     bar = st.progress(0)
     
     for i in range(100):
         latest_iteration.text(f'Analysing Text 🔎 {i+1}%')
         bar.progress(i + 1)
-        
         time.sleep(0.05)  
-
-    st.write("Analysis Complete")
     pattern = re.compile(r'(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))')
-#https://gist.github.com/gruber/8891611
+# regular expression from https://gist.github.com/gruber/8891611
     matches = pattern.findall(text)
-    if len(matches) == 1:
-      text = scrape(text)
-    predict(text)
-
-    st.write('Disclaimer⚠️ Machine Learning is not 100 percent accurate and can make mistakes❗')
-
-    st.write('Five most recent Articles with their classification:')
-    sql='SELECT* FROM Sheet1 ORDER BY Date DESC LIMIT 5' #uses sql select statement to get updated result
-    select=conn.query(sql=sql,ttl=20)
-    st.dataframe(select)
-    
-
-
-
-#Streamlit Website
-
+    if len(matches) == 1: #if link found in input it will go to llm to get article contents
+      text = str(scrape(text))
+    verify = check_db(text)
+    if verify == False:
+      stuff = predict(text)
+      st.markdown('*please wait while our Large Language Model checks the facts of this article...*')
+      try: #exception handling for llm agent becuase does not always work
+         result = agent(text)
+      except Exception as e:
+         st.write('An Error has occurred please try again later!')
+         result = 'NA' 
+      st.markdown('**Disclaimer**⚠️ Machine Learning is not 100 percent accurate and can make mistakes')
+      sentiment, sentiment_coloured = get_sentiment(text)
+      text_list = [text]
+      topics = topic(text_list)    
+      st.write(f'Additionally we found that this news article with the keywords of "{topics}" has a {sentiment_coloured} sentiment')
+      stuff.append(result)
+      stuff.append(sentiment)
+      stuff.append(topics)
+      old_data=conn.read()
+      info=pd.DataFrame(data=[stuff],columns=['Article','Classification','Date','LLM','Sentiment','Topic'])
+      concat_data = pd.concat([old_data, info], ignore_index=True)
+      conn.update(data=concat_data) #all this code is for updating db with new information, uses dataframe
     
--- pages/2_📈_Analytics.py
+++ pages/2_📈_Analytics.py
@@ -30,7 +30,7 @@ real = int(df['count(Classification)'].iloc[0])
 st.write(f'This fake news detector has identified {fake} out of {real} articles as being fake news!')
 
 option = st.selectbox(
-    "See how many fake and real articles our detector has classified over different time periods",
+    "See how many Fake and Real articles our detector has classified over different time periods",
     ("Last 7 Days", "Last 14 Days", "Last Month", "Last 6 Months", "Last Year", "All Time"))
 
 if option == 'Last 7 Days':
@@ -42,8 +42,18 @@ if option == 'Last 7 Days':
     #cast converts date column from sheets from str to date
     select=conn.query(sql=sevenDays,ttl=20)
     counts=pd.DataFrame(select['Classification'].value_counts())
-    chart_data = pd.DataFrame(counts)
-    st.bar_chart(data=chart_data, color='#0f5bd1')
+    fig, ax = plt.subplots(figsize = (10, 5))
+    colours = {'Fake':'#ff4c4c','Real':'#38d864'}
+    value_colours = [colours[x] for x in counts.index] 
+    bars = counts.plot(kind='bar', legend=False, ax=ax)
+    for bar, color in zip(bars.patches, value_colours):
+        bar.set_color(color)
+    fig.patch.set_facecolor('none')
+    ax.tick_params(labelcolor='white')
+    ax.set_facecolor('none') 
+    ax.set_xlabel("Classification")
+    ax.set_ylabel("Number of Classifications")
+    st.pyplot(fig)  
 
 elif option == 'Last 14 Days':
     fourtheenDays = """
@@ -53,8 +63,18 @@ elif option == 'Last 14 Days':
     """
     select=conn.query(sql=fourtheenDays,ttl=20)
     counts=pd.DataFrame(select['Classification'].value_counts())
-    chart_data = pd.DataFrame(counts)
-    st.bar_chart(data=chart_data, color='#0f5bd1')
+    fig, ax = plt.subplots(figsize = (10, 5))
+    colours = {'Fake':'#ff4c4c','Real':'#38d864'}
+    value_colours = [colours[x] for x in counts.index] 
+    bars = counts.plot(kind='bar', legend=False, ax=ax)
+    for bar, color in zip(bars.patches, value_colours):
+        bar.set_color(color)
+    fig.patch.set_facecolor('none')
+    ax.tick_params(labelcolor='white')
+    ax.set_facecolor('none') 
+    ax.set_xlabel("Classification")
+    ax.set_ylabel("Number of Classifications")
+    st.pyplot(fig)  
 
 elif option == 'Last Month':
     oneMonth = """
@@ -64,8 +84,18 @@ elif option == 'Last Month':
     """
     select=conn.query(sql=oneMonth,ttl=20)
     counts=pd.DataFrame(select['Classification'].value_counts())
-    chart_data = pd.DataFrame(counts)
-    st.bar_chart(data=chart_data, color='#0f5bd1')
+    fig, ax = plt.subplots(figsize = (10, 5))
+    colours = {'Fake':'#ff4c4c','Real':'#38d864'}
+    value_colours = [colours[x] for x in counts.index] 
+    bars = counts.plot(kind='bar', legend=False, ax=ax)
+    for bar, color in zip(bars.patches, value_colours):
+        bar.set_color(color)
+    fig.patch.set_facecolor('none')
+    ax.tick_params(labelcolor='white')
+    ax.set_facecolor('none') 
+    ax.set_xlabel("Classification")
+    ax.set_ylabel("Number of Classifications")
+    st.pyplot(fig)  
 
 elif option == 'Last 6 Months':
     sixMonths = """
@@ -75,8 +105,18 @@ elif option == 'Last 6 Months':
     """
     select=conn.query(sql=sixMonths,ttl=20)
     counts=pd.DataFrame(select['Classification'].value_counts())
-    chart_data = pd.DataFrame(counts)
-    st.bar_chart(data=chart_data, color='#0f5bd1')
+    fig, ax = plt.subplots(figsize = (10, 5))
+    colours = {'Fake':'#ff4c4c','Real':'#38d864'}
+    value_colours = [colours[x] for x in counts.index] 
+    bars = counts.plot(kind='bar', legend=False, ax=ax)
+    for bar, color in zip(bars.patches, value_colours):
+        bar.set_color(color)
+    fig.patch.set_facecolor('none')
+    ax.tick_params(labelcolor='white')
+    ax.set_facecolor('none') 
+    ax.set_xlabel("Classification")
+    ax.set_ylabel("Number of Classifications")
+    st.pyplot(fig)  
 
 elif option == 'Last Year':
     oneYear = """
@@ -86,15 +126,39 @@ elif option == 'Last Year':
     """
     select=conn.query(sql=oneYear,ttl=20)
     counts=pd.DataFrame(select['Classification'].value_counts())
-    chart_data = pd.DataFrame(counts)
-    st.bar_chart(data=chart_data, color='#0f5bd1')
+    fig, ax = plt.subplots(figsize = (10, 5))
+    colours = {'Fake':'#ff4c4c','Real':'#38d864'}
+    value_colours = [colours[x] for x in counts.index] 
+    bars = counts.plot(kind='bar', legend=False, ax=ax)
+    for bar, color in zip(bars.patches, value_colours):
+        bar.set_color(color)
+    fig.patch.set_facecolor('none')
+    ax.tick_params(labelcolor='white')
+    ax.set_facecolor('none') 
+    ax.set_xlabel("Classification")
+    ax.set_ylabel("Number of Classifications")
+    st.pyplot(fig)  
 
 elif option == 'All Time':
     total = 'SELECT* FROM Sheet1'
     select=conn.query(sql=total,ttl=20)
     counts=pd.DataFrame(select['Classification'].value_counts())
-    chart_data = pd.DataFrame(counts)
-    st.bar_chart(data=chart_data, color='#0f5bd1')
+    fig, ax = plt.subplots(figsize = (10, 5))
+    colours = {'Fake':'#ff4c4c','Real':'#38d864'}
+    value_colours = [colours[x] for x in counts.index] 
+    bars = counts.plot(kind='bar', legend=False, ax=ax)
+    for bar, color in zip(bars.patches, value_colours):
+        bar.set_color(color)
+    fig.patch.set_facecolor('none')
+    ax.tick_params(labelcolor='white')
+    ax.set_facecolor('none') 
+    ax.set_xlabel("Classification")
+    ax.set_ylabel("Number of Classifications")
+    st.pyplot(fig)  
+
+
+
+  
 
 retreive = "SELECT * FROM Sheet1"
 select=conn.query(sql=retreive,ttl=20)
@@ -105,14 +169,60 @@ real = df[df['Classification'] == 'Real']['Article']
 st.header('Most common words in Fake News articles:')
 text = " ".join(t for t in fake)
 word_cloud = WordCloud(collocations = False, background_color = 'black').generate(text)
-plt.imshow(word_cloud, interpolation='bilinear')
-plt.axis("off")
-st.pyplot(plt)  
+fig, ax = plt.subplots()
+ax.imshow(word_cloud, interpolation='bilinear')
+ax.axis("off")
+st.pyplot(fig)  
 
 st.header('Most common words in Real News articles:')
 text = " ".join(t for t in real)
 word_cloud = WordCloud(collocations = False, background_color = 'black').generate(text)
-plt.imshow(word_cloud, interpolation='bilinear')
-plt.axis("off")
-st.pyplot(plt)   
+fig, ax = plt.subplots()
+ax.imshow(word_cloud, interpolation='bilinear')
+ax.axis("off")
+st.pyplot(fig)   
 
+#all
+st.header('Sentiment Analysis:')
+option = st.selectbox(
+    "Find out the Sentiments of the Articles that our model has classified",
+    ("All Articles", "Fake Articles", "Real Articles"))
+
+if option == 'All Articles':
+    retreive = "SELECT Sentiment FROM Sheet1"
+    select=conn.query(sql=retreive,ttl=20)
+    counts=select.value_counts().sort_values(ascending=True) #sorts the counts of each sentiment
+    fig, ax = plt.subplots(figsize=(11, 6))
+    colours = {('Negative',):'#ff4c4c',('Positive',):'#38d864',('Neutral',):'#ffff00'}
+    value_colours = [colours[x] for x in counts.index] #makes sure that colours stay the same for sentiments    
+    fig.patch.set_facecolor('none')
+    ax.set_facecolor('none')   
+    counts.plot.pie(ax=ax,autopct='%1.1f%%', colors=value_colours,textprops={'color':"black"}, labels=None)
+    ax.axis('equal')
+    st.pyplot(fig)
+
+elif option == 'Fake Articles':
+    retreive = "SELECT Sentiment FROM Sheet1 WHERE Classification == 'Fake'"
+    select=conn.query(sql=retreive,ttl=20)
+    counts=select.value_counts().sort_values(ascending=True) #sorts the counts of each sentiment
+    fig, ax = plt.subplots(figsize=(11, 6))
+    colours = {('Negative',):'#ff4c4c',('Positive',):'#38d864',('Neutral',):'#ffff00'}
+    value_colours = [colours[x] for x in counts.index] #makes sure that colours stay the same for sentiments    
+    fig.patch.set_facecolor('none')
+    ax.set_facecolor('none')   
+    counts.plot.pie(ax=ax,autopct='%1.1f%%', colors=value_colours,textprops={'color':"black"}, labels=None)
+    ax.axis('equal')
+    st.pyplot(fig)
+
+elif option == 'Real Articles':
+    retreive = "SELECT Sentiment FROM Sheet1 WHERE Classification == 'Real'"
+    select=conn.query(sql=retreive,ttl=20)
+    counts=select.value_counts().sort_values(ascending=True) #sorts the counts of each sentiment
+    fig, ax = plt.subplots(figsize=(11, 6))
+    colours = {('Negative',):'#ff4c4c',('Positive',):'#38d864',('Neutral',):'#ffff00'}
+    value_colours = [colours[x] for x in counts.index] #makes sure that colours stay the same for sentiments    
+    fig.patch.set_facecolor('none')
+    ax.set_facecolor('none')   
+    counts.plot.pie(ax=ax,autopct='%1.1f%%', colors=value_colours,textprops={'color':"black"}, labels=None)
+    ax.axis('equal')
+    st.pyplot(fig)
