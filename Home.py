6e181b079d04be5887271e5528bb090cb51aee7a
Nathan Bishop <149484364+Nathb2001@users.noreply.github.com>
2024-07-23 15:54:34 +0100

    Update Home.py
    added some features while waiting for predictions

--- Home.py
+++ Home.py
@@ -41,6 +41,8 @@ def load_model():
     with open('fakenews_model.joblib', 'rb') as joblib_in:
         model = joblib.load(joblib_in)
     return model
+
+@st.cache_data(show_spinner=False)  
 def check_db(text):
     '''checks if text exists in database by using exception handling if not exists will be run 
     through ml and llm model as an error will be raised. If it exists the information from db gets 
@@ -71,6 +73,7 @@ def check_db(text):
     except:
       return False
    
+@st.cache_data(show_spinner=False)
 def scrape(text):
   '''
   uses libraries of requests to load the page and bs4 to parse the html text. This goes to a LLM 
@@ -90,7 +93,7 @@ def scrape(text):
   claims=claims.replace('\n','')
   return claims
 
-@st.cache_data() 
+@st.cache_data(show_spinner='Thinking...') 
 def predict(data):
     '''
     Loads logistic regression model, turns user input article to dataframe and preprocesses it.
@@ -170,7 +173,8 @@ def llm_agent(chat, q):
     In the explanation right that the Fact Claim checker has concluded if it is Real or if the article is Fake, this is important
     You need to explain in 50 words why the article is either fake or real. """
   return agent.run(input.format(statement=q))
-  
+
+@st.cache_data(show_spinner='checking facts...')
 def agent(article):
   '''
   LLM gets article and decides the claims from it that need fact checking then sends it to the 
@@ -263,7 +267,7 @@ st.set_page_config(
     page_icon="🕵️‍♂️",
 )
 
-conn = st.connection("gsheets",type=GSheetsConnection) #loading connection to db
+#conn = st.connection("gsheets",type=GSheetsConnection) #loading connection to db
 st.write("# Fake News Detector 🕵️‍♂️")
 text = st.text_input("Enter an Article or an Article Link here:", key="Article")
 st.write('Hint💡: Try to enter as much of the news article contents as possible and to not include information that is not related to the article.')
@@ -275,7 +279,7 @@ if text:
     for i in range(100):
         latest_iteration.text(f'Analysing Text 🔎 {i+1}%')
         bar.progress(i + 1)
-        time.sleep(0.05)  
+        time.sleep(0.01)  
     pattern = re.compile(r'(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))')
 # regular expression from https://gist.github.com/gruber/8891611
     matches = pattern.findall(text)
@@ -286,10 +290,10 @@ if text:
       stuff = predict(text)
       st.markdown('*please wait while our Large Language Model checks the facts of this article...*')
       try: #exception handling for llm agent becuase does not always work
-          result = agent(text)
+         result = agent(text)
       except Exception as e:
-          st.write('An Error has occurred please try again later!')
-          result = 'NA' 
+         st.write('An Error has occurred please try again later!')
+         result = 'NA' 
       st.markdown('**Disclaimer**⚠️ Machine Learning is not 100 percent accurate and can make mistakes')
       sentiment, sentiment_coloured = get_sentiment(text)
       text_list = [text]
